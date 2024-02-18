0b84c9e42cd54c51dcc47fa1ab25b9cf9b1df198
huskyroll <developer@huskyroll.dev>
2024-02-18 00:34:41 -0600

    Configuration Break Up
    Starting the process of breaking up the plugin configuration
    files.

--- lua/huskyroll/init.lua
+++ lua/huskyroll/init.lua
@@ -13,63 +13,14 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
+local opts = {}
 
-local plugins = {
-    {
-        'folke/tokyonight.nvim', 
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 
-            'nvim-lua/plenary.nvim',
-        }
-    },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate'
-    },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
-            "3rd/image.nvim", 
-        }
-    }
-}
-local ops = {}
-
-require("lazy").setup(plugins, opts)
-
-local builtin =  require("telescope.builtin")
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local configs = require("nvim-treesitter.configs")
-configs.setup({
-    ensure_installed = {
-        'lua',
-        'javascript',
-        'html',
-        'go',
-        'swift',
-        'gdscript',
-        'python',
-        'json',
-        'dockerfile',
-        'css',
-        'cpp',
-        'yaml'
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
+require("lazy").setup({
+    spec = 'huskyroll.plugins'
 })
 
 
-require("tokyonight").setup()
-vim.cmd.colorscheme("tokyonight")
+
+
+vim.keymap.set('n', '<leader>neo', ':Neotree filesystem reveal left<CR>')
+vim.keymap.set('n', '<leader>neoc', ':Neotree filesystem close<CR>')
---
+++ lua/huskyroll/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = {
+                'lua',
+                'javascript',
+                'html',
+                'go',
+                'swift',
+                'gdscript',
+                'python',
+                'json',
+                'dockerfile',
+                'css',
+                'cpp',
+                'yaml'
+            },
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
+
