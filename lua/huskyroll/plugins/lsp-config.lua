978494671adcb902714dbd754d330acd89a07f5a
huskyroll <developer@huskyroll.dev>
2024-02-19 07:06:56 -0600

    LSP-Config
    Added the bare blocks of LSP-config

--- lua/huskyroll/plugins/lsp-config.lua
+++ lua/huskyroll/plugins/lsp-config.lua
@@ -15,5 +15,12 @@ return {
                 }
             })
         end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({})
+        end
     }
 }
