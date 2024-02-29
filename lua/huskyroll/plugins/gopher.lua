098432eb85029ec03326d0252a58e9c692b060de
huskyroll <developer@huskyroll.dev>
2024-02-28 20:12:39 -0600

    Returning config to main
    Returning the config to use the main branch on the gopher plugin
    due to the changes not quite working.

--- lua/huskyroll/plugins/gopher.lua
+++ lua/huskyroll/plugins/gopher.lua
@@ -1,6 +1,5 @@
 return {
 	"huskyroll/gopher.nvim",
-	branch = "add-split-and-run",
     ft = "go", "gomod",
 	config = function()
 		require("gopher").setup()
--- lua/huskyroll/plugins/none-ls.lua
+++ lua/huskyroll/plugins/none-ls.lua
@@ -5,7 +5,9 @@ return {
 
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.stylua.with({
+					filetypes = { "lua" },
+				}),
 				null_ls.builtins.diagnostics.flake8.with({
 					extra_args = {
 						"--max-line-length",
@@ -15,8 +17,12 @@ return {
 				null_ls.builtins.diagnostics.actionlint,
 				null_ls.builtins.diagnostics.eslint,
 				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.gofmt,
-				null_ls.builtins.formatting.goimports_reviser,
+				null_ls.builtins.formatting.gofmt.with({
+					filetypes = { "go" },
+				}),
+				null_ls.builtins.formatting.goimports_reviser.with({
+					filetypes = { "go" },
+				}),
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
