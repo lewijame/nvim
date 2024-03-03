102c48573c2b1eac10f88b82f666fc7a24cd1857
Matt Lewis <rancored_mongoose@outlook.com>
2024-03-03 12:25:37 -0600

    Updated LSP Config
    Updated the HTML LSP Config in hopes of making it work for
    templ and tmpl files.

--- lua/huskyroll/plugins/lsp-config.lua
+++ lua/huskyroll/plugins/lsp-config.lua
@@ -31,6 +31,14 @@ return {
             lspconfig.tsserver.setup({
                 capabilities = capabilities,
             })
+            lspconfig.html.setup({
+                capabilities = capabilities,
+                filetypes = {
+                    "html",
+                    "templ",
+                    "tmpl",
+                },
+            })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
